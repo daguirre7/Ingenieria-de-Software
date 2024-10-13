155685229eeee676036f3cfcaaafe1ec8c3ef08a
daguirre7 <daaguirre777@gmail.com>
2024-10-12 20:08:56 -0600

    2.8.1-3.C Creación de página Equipo de Trabajo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Equipo from './components/Equipo';
 
 function App() {
   return (
    <BrowserRouter>
+
    <Routes>
-    <Route path='/' element={<Login/>}></Route>
+      <Route path='/' element={<Login/>}/>
+      <Route path="equipo" element={<Equipo/>}/>
    </Routes>
+   
    </BrowserRouter> 
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./components/styles.css"
+import "./components/Equipo.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
